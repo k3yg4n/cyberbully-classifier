77d6bdd67fea2a5ae034423320c7f5b50d05f600
Keegan Liu <keeganliu@eduroam-campus-10-36-93-222.campus-dynamic.uwaterloo.ca>
2025-07-28 14:04:41 -0400

    Update logistic_regression_nameerah to use new clean_data.csv

--- logistic_regression_nameerah/logistic_regression.py
+++ logistic_regression_nameerah/logistic_regression.py
@@ -11,54 +11,66 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
 
-CSV_PATH = '../data/train_subset_clean.csv'
+CSV_PATH = "../data/output/clean_data.csv"
 TEST_SIZE = 0.2
 RANDOM_STATE = 42
-STRATIFY_LABEL = 'threat' # Stratify to ensure the train/test splits have the same relative frequency of toxic vs. non‑toxic
+STRATIFY_LABEL = "cyberbullying"  # Stratify to ensure the train/test splits have the same relative frequency of toxic vs. non‑toxic
 
 # Load & read column names
 df = pd.read_csv(CSV_PATH)
-X = df['clean_comment'].astype(str)
-Y = df[['toxic', 'insult', 'threat', 'discrimination']].astype(int)
+X = df["comment_text"].astype(str)
+Y = df["cyberbullying"].astype(int)
 
 # Train/test split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, Y,
-    test_size=TEST_SIZE,
-    random_state=RANDOM_STATE,
-    stratify=Y[STRATIFY_LABEL]
+    X, Y, test_size=TEST_SIZE, random_state=RANDOM_STATE, stratify=Y
 )
 
 # Use TF-IDF to turn text into numbers + try a logistic regression model
-pipeline = Pipeline([
-    ("tfidf", TfidfVectorizer(
-        preprocessor=None,
-        stop_words=None,
-        max_features=20000,
-        ngram_range=(1,2)
-    )),
-    ("clf", MultiOutputClassifier(
-        LogisticRegression(
-            class_weight="balanced",
-            max_iter=1000,
-            random_state=RANDOM_STATE
-        )
-    ))
-])
+pipeline = Pipeline(
+    [
+        (
+            "tfidf",
+            TfidfVectorizer(
+                preprocessor=None,
+                stop_words=None,
+                max_features=20000,
+                ngram_range=(1, 2),
+            ),
+        ),
+        (
+            "clf",
+            LogisticRegression(
+                class_weight="balanced", max_iter=1000, random_state=RANDOM_STATE
+            ),
+        ),
+    ]
+)
 
 # Parameters
 params = {
-    "tfidf__max_df":        [0.9, 0.95, 1.0], # Drop terms that appear in more than this fraction of documents (too common)
-    "tfidf__ngram_range":   [(1,1), (1,2)], # Whether to use just unigrams (1,1) or unigrams + bigrams (1,2)
-    "clf__estimator__C":     [0.1, 1.0, 10.0] # Amount of regularizatioon - bigger C = less regularization
+    "tfidf__max_df": [
+        0.9,
+        0.95,
+        1.0,
+    ],  # Drop terms that appear in more than this fraction of documents (too common)
+    "tfidf__ngram_range": [
+        (1, 1),
+        (1, 2),
+    ],  # Whether to use just unigrams (1,1) or unigrams + bigrams (1,2)
+    "clf__C": [
+        0.1,
+        1.0,
+        10.0,
+    ],  # Amount of regularizatioon - bigger C = less regularization
 }
 grid = GridSearchCV(
-    estimator = pipeline, # TF-IDF + logistic regression pipeline
-    param_grid = params,
-    cv         = 3, # Use 3 fold cross validation
-    scoring    = "f1_macro", # Trying to maximize f1
-    n_jobs     = -1, # Use all CPU cores
-    verbose    = 2 # print progress
+    estimator=pipeline,  # TF-IDF + logistic regression pipeline
+    param_grid=params,
+    cv=3,  # Use 3 fold cross validation
+    scoring="f1_macro",  # Trying to maximize f1
+    n_jobs=-1,  # Use all CPU cores
+    verbose=2,  # print progress
 )
 
 # Train and evaluate model on every combo in param_grid suing 3-fold CV, keep the best f1
@@ -73,32 +85,31 @@ print("\nEvaluating on test set: ")
 best_model = grid.best_estimator_
 y_pred = best_model.predict(X_test)
 
-for i, label in enumerate(Y.columns):
-    print(f"\n--- {label.upper()} ---")
-    print(classification_report(
-        y_test[label],
-        y_pred[:, i],
-        zero_division=0
-    ))
+label = "cyberbullying"
+print(f"\n--- {label.upper()} ---")
+print(classification_report(y_test, y_pred, zero_division=0))
+
+# for i, label in enumerate(Y.columns):
+#     print(f"\n--- {label.upper()} ---")
+#     print(classification_report(y_test[label], y_pred[:, i], zero_division=0))
 
 # Confusion matrix
-labels = y_test.columns.tolist() # ['toxic','insult','threat','discrimination']
+# labels = y_test.columns.tolist()  # ['cyberbullying']
+# for i, lbl in enumerate(labels):
+#     y_true = y_test[lbl].values
+#     y_predicted = y_pred[:, i]
 
-for i, lbl in enumerate(labels):
-    y_true = y_test[lbl].values
-    y_predicted = y_pred[:, i]
+#     cm = confusion_matrix(y_true, y_predicted, labels=[0, 1])
+#     print(f"\n--- {lbl.upper()} ---")
+#     print(pd.DataFrame(cm, index=["true_0", "true_1"], columns=["pred_0", "pred_1"]))
 
-    cm = confusion_matrix(y_true, y_predicted, labels=[0,1])
-    print(f"\n--- {lbl.upper()} ---")
-    print(pd.DataFrame(cm, index=['true_0','true_1'], columns=['pred_0','pred_1']))
+#     # Plot
+#     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0, 1])
+#     disp.plot(values_format="d")
+#     plt.title(lbl.upper())
+#     plt.show()
 
-    # Plot
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0,1])
-    disp.plot(values_format='d')
-    plt.title(lbl.upper())
-    plt.show()
-
-'''
+"""
 Evaluating on test set: 
 
 Evaluating on test set: 
@@ -172,4 +183,4 @@ TO IMPROVE:
     - oversample these classes?
 - Try a different linear model --> LinearSVC does slightly better on text
 
-'''
+"""
